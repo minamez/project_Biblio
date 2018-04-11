3abd18c1017a7f712fdac38382451ca9de0b3d93
xGilgax <jjmatthieu@hotmail.fr>
2018-04-11 16:15:55 +0200

    branche matthieu

--- src/main/java/ProjetEcole1/ProjectBiblio/IhmEmprunt.java
+++ src/main/java/ProjetEcole1/ProjectBiblio/IhmEmprunt.java
@@ -17,7 +17,7 @@ public class IhmEmprunt {
 	private JTextField textField;
 
 	/**
-	 * Launch the application.
+	 * Launch the application..
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
